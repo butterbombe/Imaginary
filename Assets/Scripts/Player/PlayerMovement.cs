3e76ee77a8eefb22ce4f6ece00e3673bf72f3dff
Butterbombe <butterbombe7@gmail.com>
2022-03-21 00:10:31 +0100

    Back to Unity Physicsbased PlayerController

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Player Movement")]
+    [SerializeField] float moveSpeed;
+    [SerializeField] float jumpVelocity;
+    [SerializeField] float lowJumpMultiplier = 2.5f;
+    [SerializeField] float fallMultiplier = 2f;
+
+    [Header("Grounding")]
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] Transform bottomLeft;
+    [SerializeField] Transform bottomRight;
+
+    private Rigidbody2D rb;
+    private float moveX;
+    private bool isGrounded;
+
+
+    private void Start()
+    {
+
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+    private void Update()
+    {
+        GroundCheck();
+        GetInput();
+        Jump();
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    public void GetInput()
+    {
+        moveX = Input.GetAxis("Horizontal");
+    }
+    private void Move()
+    {
+        transform.Translate((new Vector3(moveX, 0) * moveSpeed) * Time.deltaTime);
+    }
+    private void Jump()
+    {
+        if (!Input.GetButtonDown("Jump"))
+        {
+            return;
+        }
+        if (!isGrounded)
+        {
+            return;
+        }
+        rb.velocity = Vector2.up * jumpVelocity;
+    }
+    private void GroundCheck()
+    {
+        isGrounded = Physics2D.OverlapArea(bottomLeft.position, bottomRight.position, groundLayer);
+    }
+}
